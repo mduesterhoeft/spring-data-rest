c46df6e66b1bfeb27f81d9e7c03e15b699f1019a
Mathias Düsterhöft <mduesterhoeft@epages.com>
2017-02-10 09:12:14 +0100

    reproduce DATAREST-1006

--- spring-data-rest-webmvc/src/test/java/org/springframework/data/rest/webmvc/json/DomainObjectReaderUnitTests.java
+++ spring-data-rest-webmvc/src/test/java/org/springframework/data/rest/webmvc/json/DomainObjectReaderUnitTests.java
@@ -15,9 +15,15 @@
  */
 package org.springframework.data.rest.webmvc.json;
 
-import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
+import static com.fasterxml.jackson.annotation.JsonProperty.Access.READ_ONLY;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.Matchers.sameInstance;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
 
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -58,6 +64,7 @@ import org.springframework.data.rest.webmvc.mapping.Associations;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
@@ -192,6 +199,22 @@ public class DomainObjectReaderUnitTests {
 		assertThat(result.firstname, is(nullValue()));
 		assertThat(result.id, is(1L));
 		assertThat(result.version, is(1L));
+	}
+
+	@Test //
+	public void doesNotWipeReadOnlyJsonPropertyForPut() throws Exception {
+
+		SampleUser sampleUser = new SampleUser("name", "password");
+		sampleUser.lastLogin = new Date();
+
+		ObjectMapper mapper = new ObjectMapper();
+		ObjectNode node = (ObjectNode) mapper.readTree("{ \"name\" : \"another\" }");
+
+		SampleUser result = reader.readPut(node, sampleUser, mapper);
+
+		assertThat(result.name, is("another"));
+		assertThat(result.password, notNullValue());
+		assertThat(result.lastLogin, notNullValue());
 	}
 
 	@Test // DATAREST-873
@@ -509,6 +532,9 @@ public class DomainObjectReaderUnitTests {
 		String name;
 		@JsonIgnore String password;
 		Map<String, SampleUser> relatedUsers;
+
+		@JsonProperty(access = READ_ONLY)
+		private Date lastLogin;
 
 		public SampleUser(String name, String password) {
 
